919e88fc9d302681b7fd20b8edfd266b72bf1c5e
zhoukai <zk3366386@163.com>
2024-07-29 17:47:31 +0800

    fis

--- src/js/prizes.js
+++ src/js/prizes.js
@@ -1,11 +1,11 @@
 export const prizes = [
     { name: "未中奖", type: 0, imgUrl: require("@/assets/err.png"), weight: 20},
-    { name: "防静电钢化膜", type: 1, imgUrl: require("@/assets/3.png"), weight: 10 },
+    { name: "apple watch 50元优惠卷", type: 1, imgUrl: require("@/assets/q50.png"), weight: 10 },
     { name: "防窥钢化膜", type: 1, imgUrl: require("@/assets/4.png"), weight: 5 },
     { name: "漫甘娜潮玩美妆礼袋", type: 1, imgUrl: require("@/assets/1.png"), weight: 5 },
     { name: "未中奖", type: 0, imgUrl: require("@/assets/err.png"), weight: 20 },
     { name: "漫甘娜游戏币", type: 1, imgUrl: require("@/assets/2.jpg"), weight: 10 },
-    { name: "apple watch 50元优惠卷", type: 1, imgUrl: require("@/assets/q50.png"), weight: 10 },
+    { name: "防静电钢化膜", type: 1, imgUrl: require("@/assets/3.png"), weight: 10 },
     { name: "iphone 15 100元优惠卷", type: 1, imgUrl: require("@/assets/q100.jpg"), weight: 10 },
     { name: "未中奖", type: 0, imgUrl: require("@/assets/err.png"), weight: 20 },
   ];
